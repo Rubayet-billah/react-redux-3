35993827b2f805e0b1c28aa22b6ccbd271c8edbe
Rubayet-billah <rubayetbillah909@gmail.com>
2023-01-17 01:54:22 +0600

    jbdfng

--- src/features/counter/CounterView.js
+++ src/features/counter/CounterView.js
@@ -4,6 +4,7 @@ const CounterView = () => {
     return (
         <div>
             <h1>Hi There! <br /> This is our counter app</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa et recusandae error non quo officia soluta harum nobis voluptas, minima nihil alias, possimus voluptatibus sed ducimus. Vel, sit unde!</p>
         </div>
     );
 };
